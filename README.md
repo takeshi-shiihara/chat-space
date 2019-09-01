9dd91373596994b530bb80c40011f6fc4d831ea4
takeshi-shiihara <shi4490@icloud.com>
2019-09-01 17:55:50 +0900

    log_in OK

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
-## groups_usersテーブル
+## group_usersテーブル
 
 |Column|Type|Options|
 |------|----|-------|
---
+++ app/models/group.rb
@@ -0,0 +1,6 @@
+class Group < ApplicationRecord
+
+  has_many :group_users
+  has_many :users , through: :groups_users
+  validates :name, presence: true, uniqueness: true
+end
