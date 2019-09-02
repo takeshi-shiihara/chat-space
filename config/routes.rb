b32aede525dcd8a83f2b27886983efaa6aa7b470
takeshi-shiihara <shi4490@icloud.com>
2019-09-02 17:59:13 +0900

    グループコミット忘れてた（一部メッセ機能付き）

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :messages, only: [:index]
-  resources :users, only: [:edit, :update]
-    
+  devise_for :users
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, onry: [:new, :create, :edit, :update] do
+    resources :messages, onry: [:index, :create]
+  end
 end
