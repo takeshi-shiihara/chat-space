21da589e1292b8027fbfe20aafcbe61510a4e023
takeshi-shiihara <shi4490@icloud.com>
2019-09-07 20:21:49 +0900

    resetメソッド書き換え

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -33,10 +33,9 @@ $(function() {
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('.form__message').val('');
+      $("#new_message")[0].reset();
       $('.form__submit').prop('disabled', false);
-      $('.messages').animate({scrollTop: $('.message')[0].scrollHeight});
-      return false
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
     })
     .fail(function(){
       alert('error')
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,5 @@ class Message < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, unless: :image?
-  
+  mount_uploader :image, ImageUploader
 end
