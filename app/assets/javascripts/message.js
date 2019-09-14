53fb981403f0bbb563e5a89428fef2bc458e1a32
takeshi-shiihara <shi4490@icloud.com>
2019-09-14 18:41:20 +0900

    修正案３

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function() {
   function buildHTML(message){
-    var image = message.image ? `<img src="${message.image}" class="lower-message__image" >` : '';
+    var image = ( message.image ) ? `<img src="${message.image}" class="lower-message__image" >` : '';
 
     var html = `<div class="message"  data-message-id="${message.id}">
                   <div class="upper-message">
