175a9c303bc9cd531d3f1cd65215fdd5a09b5a26
takeshi-shiihara <shi4490@icloud.com>
2019-09-15 02:06:30 +0900

    修正案７

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,8 @@ class Message < ApplicationRecord
   
   belongs_to :group
   belongs_to :user
+
+  mount_uploader :image, ImageUploader
   
   validates :content, presence: true, unless: :image?
-  mount_uploader :image, ImageUploader
 end
